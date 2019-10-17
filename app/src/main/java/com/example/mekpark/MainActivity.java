e5296828edd47e53b872e6a6ead39ddb49cef120
Srishti Rohatgi <s.rohatgi23@gmail.com>
2019-10-17 17:54:24 +0530

    updated

--- app/src/main/java/com/example/mekpark/MainActivity.java
+++ app/src/main/java/com/example/mekpark/MainActivity.java
@@ -1,33 +1,32 @@
 package com.example.mekpark;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_account_login);
 
-
+        login = findViewById(R.id.login);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
     }
 
-    public void OnEnter(View v){
-        Intent i = new Intent(MainActivity.this,account_login.class);
-        startActivity(i);
 
-    }
-    public void onProfile(View v){
-
-
-        Intent i = new Intent(MainActivity.this,profile.class);
-        startActivity(i);
-    }
 }
